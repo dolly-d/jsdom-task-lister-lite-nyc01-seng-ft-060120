f7dc2d19a2597f9e678d1edf65199110fc583ae7
dolly-d <dolly.desir@gmail.com>
2020-11-12 12:27:51 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const taskForm = document.getElementById("create-task-form")
+  taskForm.addEventListener("submit",function(e){
+    e.preventDefault()
+    let newTask = document.getElementById('new-task-description').value
+    
+    const ul  = document.getElementById("tasks")
+    const li = document.createElement('li')
+    ul.appendChild(li);
+    li.innerText = newTask
+    taskForm.reset();
+  })
+  
+
+
 });
+
+
+
+
